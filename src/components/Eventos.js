5390f04473fab120a756e43040a4f617ca9e46ad
rodrigo linux <rodrigomalegre@gmail.com>
2022-08-08 01:45:07 -0300

    Video 9 Curso React- Eventos (bind)

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -6,6 +6,8 @@ export default class Eventos extends Component {
     this.state = {
       contador: 0,
     };
+
+    this.sumar = this.sumar.bind(this); // comun en las class component para enlazar los elementos con (this)
   }
 
   sumar() {
@@ -14,12 +16,14 @@ export default class Eventos extends Component {
       contador: this.state.contador + 1,
     });
   }
+
   render() {
+    console.log(this);
     return (
       <div>
         <h2>Eventos</h2>
         <nav>
-          <button onClick={this.sumar.bind(this)}>+</button>
+          <button onClick={this.sumar}>+</button>
           <button>-</button>
         </nav>
         <h3>{this.state.contador}</h3>
